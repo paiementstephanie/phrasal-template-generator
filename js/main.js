5001ba3fa89e0b6c8a74b2070a9780759d1ce566
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-01 00:05:00 -0500

    added if and else if tags

--- js/main.js
+++ js/main.js
@@ -10,3 +10,10 @@ var newVerb = prompt('A past tense verb (ran, burped, etc.)');
 
 var newStory = prompt('Which story would you like? a) Snacking b) Escaping c) Winning');
 
+if (newStory == 'a') {
+	alert('After snacking on ' + newName ' treats, the ' + newColour ' bellied ' + newCreature + ' ' + newVerb' for hours.');
+} else if (newStory == 'b') {
+		alert('Ameilia' + newVerb ' through the' + newAdjective + newColour ' nebula escaping the space' + newCreature);
+} else if (newStory == 'c') {
+		alert('Jackson chose his ' + newAdjective ',' + newColour ' ' + newCreature ' card and ' + newVerb ' it to the table knowing he won.');
+}

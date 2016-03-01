54aec4e5c0a62343efa17bf4014e4ca938228b78
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-01 00:04:44 -0500

    added var prompts

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var newTitle = prompt('Let’s make a story together!');
+
+var newColour = prompt('A colour (orange, indigo, etc.)');
+
+var newCreature = prompt('A creature (dragon, moth, etc.)');
+
+var newAdjective = prompt('An adjective (beautiful, super, etc.)');
+
+var newVerb = prompt('A past tense verb (ran, burped, etc.)');
+
+var newStory = prompt('Which story would you like? a) Snacking b) Escaping c) Winning');
+
